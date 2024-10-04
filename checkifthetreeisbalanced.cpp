62daa94e8d58cfb5abacabe15566f69bdb67cb80
Aman Thapliyal <kira@Amans-MacBook-Air.local>
2024-10-04 18:01:55 +0530

    Added balanced binary tree

---
+++ checkifthetreeisbalanced.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int solve(TreeNode* root){
+    if(root==NULL){
+        return 0;
+    }
+
+    int leftheight=solve(root->left);
+    if(leftheight==-1) return -1;
+    int rightheight=solve(root->right);
+    if(rightheight==-1) return -1;
+
+    if(abs(rightheight-leftheight)>1){
+        return -1;
+    }
+
+    return 1+max(leftheight,rightheight);
+}
+    bool isBalanced(TreeNode* root) {
+        if(solve(root)!=-1){
+            return true;
+        }
+        return false;
+    }
+};
